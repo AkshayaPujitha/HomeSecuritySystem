6c26636889e073bd9fb61d47581247ffb8737446
pujitha <akshayapujithakolli@gmail.com>
2023-07-08 15:45:37 +0530

    otp done by pyot

--- security/security/settings.py
+++ security/security/settings.py
@@ -132,5 +132,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTHENTICATION_BACKENDS = [
+    'django_otp.backends.ModelBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
+
