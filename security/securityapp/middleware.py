4ce17faeb808fe623fc9f85c1d9a9f1ef32c4ac4
pujitha <akshayapujithakolli@gmail.com>
2023-07-09 15:15:26 +0530

    modified code of middle ware

--- security/securityapp/middleware.py
+++ security/securityapp/middleware.py
@@ -5,9 +5,7 @@ class VerifyOTPRedirectMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        print(request.session.get('registration_phone_number'))
         if request.path == '/verify_otp/' and not request.session.get('registration_phone_number'):
             return redirect('register')
-
         response = self.get_response(request)
         return response
