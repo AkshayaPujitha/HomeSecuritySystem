c2aafc8819518fe843ebba153458cb21801840a1
pujitha <akshayapujithakolli@gmail.com>
2023-07-09 15:58:12 +0530

    permission class added for /dashboard

--- security/dashboard/views.py
+++ security/dashboard/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import permission_classes,authentication_classes,api_view
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def dashboard(request):
     return render(request,'dashboard.html')
