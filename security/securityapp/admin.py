839daf0fab9cab0f710ed62933749248e92af1fe
pujitha <akshayapujithakolli@gmail.com>
2023-07-09 14:50:51 +0530

    model events created and modeified

--- security/securityapp/admin.py
+++ security/securityapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser,CustomUserManager
+from .models import CustomUser,EventLog,Alarm
 # Register your models here.
 
 admin.site.register(CustomUser)
+admin.site.register(EventLog)
+admin.site.register(Alarm)
+
--- security/securityapp/models.py
+++ security/securityapp/models.py
@@ -53,10 +53,16 @@ class EventLog(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     details = models.TextField()
 
+    def __str__(self):
+        return self.event_type
+
 class Alarm(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     alarm_type = models.CharField(max_length=50)
     timestamp = models.DateTimeField(auto_now_add=True)
     details = models.TextField()
     event_log = models.ForeignKey(EventLog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.alarm_type
 
