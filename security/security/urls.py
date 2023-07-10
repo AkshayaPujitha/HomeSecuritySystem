84e202dc804935749ee0a5761330cf80d6c93f4e
pujitha <akshayapujithakolli@gmail.com>
2023-07-10 17:21:11 +0530

    media root added

--- security/security/urls.py
+++ security/security/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static 
 
 
 urlpatterns = [
@@ -25,3 +27,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     
 ]
+if settings.DEBUG:
+        print("here")
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
