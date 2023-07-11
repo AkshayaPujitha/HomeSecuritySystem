4d7093daca3c834173ebf07723ce9e821620a46c
pujitha <akshayapujithakolli@gmail.com>
2023-07-11 16:41:57 +0530

    static root added

--- security/security/settings.py
+++ security/security/settings.py
@@ -131,9 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
--- security/security/urls.py
+++ security/security/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     
 ]
 if settings.DEBUG:
-        print("here")
         urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
