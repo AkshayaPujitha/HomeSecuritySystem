3edfd0a07a85b3892e2ddaab51ccb205e3d3a79e
pujitha <akshayapujithakolli@gmail.com>
2023-07-16 23:55:47 +0530

    changes i shell script used to create env

--- run_project.sh
+++ run_project.sh
@@ -1,4 +1,11 @@
+#Create Virtual environment
+virtualenv secenv -p python3
+
+#Activate virtual environment
 source secenv/bin/activate
+
+#Install Dependencies
+pip install -r requirements.txt
 
 # Change to your project directory
 cd security
