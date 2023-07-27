66db421d49273ebb7d8740487bcc98b317eccd44
pujitha <akshayapujithakolli@gmail.com>
2023-07-27 19:24:41 +0530

    alarms and intrusion be deleted after 30 days

--- security/dashboard/signals.py
+++ security/dashboard/signals.py
@@ -4,9 +4,19 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from datetime import timedelta
 from django.utils import timezone
-from dashboard.models import EventLog
+from dashboard.models import EventLog,Alarm,IntrusionImage
 
 @receiver(post_save, sender=EventLog)
 def delete_old_events(sender, instance, **kwargs):
     thirty_days_ago = timezone.now() - timedelta(days=30)
     EventLog.objects.filter(timestamp=thirty_days_ago).delete()
+
+@receiver(post_save, sender=Alarm)
+def delete_old_events(sender, instance, **kwargs):
+    thirty_days_ago = timezone.now() - timedelta(days=30)
+    Alarm.objects.filter(timestamp=thirty_days_ago).delete()
+
+@receiver(post_save, sender=IntrusionImage)
+def delete_old_events(sender, instance, **kwargs):
+    thirty_days_ago = timezone.now() - timedelta(days=30)
+    IntrusionImage.objects.filter(timestamp=thirty_days_ago).delete()
