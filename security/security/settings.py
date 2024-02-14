bbc7c28279b8042fe90eb94591456ab8cad720d3
pujitha <akshayapujithakolli@gmail.com>
2024-02-14 19:14:58 +0530

    environment variables set for the deployment

--- security/security/settings.py
+++ security/security/settings.py
@@ -27,10 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y&g_wg0guv4uq#w!sjr&@-s+gk=we4fz@pm_b-1-z2nk%0mieu"
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG',"False").lower()=="true"
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'securityapp.CustomUser'
